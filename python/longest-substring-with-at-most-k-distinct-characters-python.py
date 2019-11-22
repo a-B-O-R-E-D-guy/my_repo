db393cab29456913c708b8ac75d0cb8cc708db14
a-B-O-R-E-D-guy <58049089+a-B-O-R-E-D-guy@users.noreply.github.com>
2019-11-22 17:26:18 -0600

    Update longest-substring-with-at-most-k-distinct-characters-python.py

--- python/longest-substring-with-at-most-k-distinct-characters-python.py
+++ python/longest-substring-with-at-most-k-distinct-characters-python.py
@@ -1,6 +1,6 @@
 s = input('substring: ')
 k = int(input('k value: '))
 li = []
-[li.append(s[i:i + k+j]) for i in range(len(s)) for j in range(len(s)-k+1)]
+[li.append(s[i:i + k+j]) for i in range(len(s)-k+1) for j in range(len(s)-k+1-i)]
 [li.remove(li[j]) for j in list(range(len(li)))[::-1] if len(set(li[j])) != k]
 print(sorted(li, key=len)[len(li) - 1])
